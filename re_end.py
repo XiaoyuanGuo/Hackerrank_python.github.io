3d3a375c3ea2f144c3ba667708493d45540c05a1
Sophie <xiaoyuanguo.ucas@gmail.com>
2019-01-15 16:25:57 -0500

    Create re_end.py

---
+++ re_end.py
@@ -0,0 +1,13 @@
+
+import re
+
+S = input()
+k = input()
+pattern = re.compile(k)
+m = pattern.search(S)
+
+if not m:
+    print("(-1, -1)")
+while m:
+    print("({0}, {1})".format(m.start(), m.end() - 1))
+    m = pattern.search(S,m.start() + 1)
